154939ece1519e89a402eebc46780ab4404e6fa6
leduongkhang <khangnhonmy@gmail.com>
2025-08-27 20:41:56 +0700

    cập nhật code

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -345,13 +345,14 @@
         
     </footer><!--/Footer-->
     
-
-  
     <script src="{{asset('public/frontend/js/jquery.js')}}"></script>
-    <script src="{{asset('public/frontend/js/bootstrap.min.js')}}"></script>
     <script src="{{asset('public/frontend/js/jquery.scrollUp.min.js')}}"></script>
-    <script src="{{asset('public/frontend/js/price-range.js')}}"></script>
     <script src="{{asset('public/frontend/js/jquery.prettyPhoto.js')}}"></script>
+    
+    <script src="{{asset('public/frontend/js/bootstrap.min.js')}}"></script>
+    
+    <script src="{{asset('public/frontend/js/price-range.js')}}"></script>
+    
     <script src="{{asset('public/frontend/js/main.js')}}"></script>
     <script src="{{asset('public/frontend/js/sweetalert.min.js')}}"></script>
     
